83368471e3470050b2ddbbe2d761f640653ca81b
LiwinLi23 <liwinli@icloud.com>
2021-06-09 21:39:18 +0800

    Add ConcurrencyVSSerial test.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,19 @@
 import com.liwinli.basic.object.flow.init.ParentClass;
 import com.liwinli.basic.object.flow.init.SubClass;
+import com.liwinli.concurrency.ConcurrencyVSSerial;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("+++++++++++++++++ This is a java program ++++++++++++++++++++++");
 //        LTFile.listPath("/Volumes/KINGSTON/image");
 //        ParentClass p = new ParentClass();
+        ConcurrencyVSSerial.test(10000);
+        ConcurrencyVSSerial.test(100000);
+        ConcurrencyVSSerial.test(1000000);
+        ConcurrencyVSSerial.test(10000000);
+        ConcurrencyVSSerial.test(100000000);
         System.out.println("---------------------------------------------------------------");
 //        SubClass s = new SubClass();
     }
---
+++ src/main/java/com/liwinli/concurrency/ConcurrencyVSSerial.java
@@ -0,0 +1,39 @@
+package com.liwinli.concurrency;
+
+public class ConcurrencyVSSerial {
+    private static long sCount = 10000;
+
+    private static void concurrencyTest() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        Thread subShread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (int i = 0; i < sCount; ++i) { ++a; }
+            }
+        });
+        subShread.start();
+        int b = 0;
+        for (int i = 0; i < sCount; ++i) { --b; }
+        subShread.join();
+        long durationTime = System.currentTimeMillis() - startTime;
+        System.out.println("ConcurrentTest duration: " + durationTime);
+    }
+
+    private static void serialsTest() {
+        long startTime = System.currentTimeMillis();
+        int a = 0;
+        for (int i = 0; i < sCount; ++i) { ++a; }
+
+        int b = 0;
+        for (int i = 0; i < sCount; ++i) { --b; }
+        long durationTime = System.currentTimeMillis() - startTime;
+        System.out.println("SerialTest duration: " + durationTime);
+    }
+
+    public static void test(long countParam) throws InterruptedException {
+        sCount = countParam;
+        concurrencyTest();
+        serialsTest();
+    }
+}
